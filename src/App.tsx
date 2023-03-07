07dab5377d596ef2178758779e93ed7a0d1e762b
Dzaky Badawi <dzaky.badawi@gmail.com>
2023-03-07 22:20:57 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <main className=" text-lime-400 text-center flex flex-1 min-h-screen justify-center items-center bg-gray-800">
+      Intilink Dashboard Starter
+    </main>
+  );
+};
+
+export default App;
