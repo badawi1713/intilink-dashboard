a7509299d0c9b01dc4c44362080f2313991eec61
Dzaky <dzaky.badawi@gmail.com>
2023-07-30 18:46:28 +0700

    feat: 🎸 add git cz to write git commit better

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,5 @@
 const configurations = {
-  parserPreset: "conventional-changelog-conventionalcommits",
-  // extends: ["@commitlint/config-conventional"],
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "body-leading-blank": [1, "always"],
     "body-max-line-length": [2, "always", 100],
@@ -136,3 +135,4 @@ const configurations = {
 };
 
 export default configurations;
+
